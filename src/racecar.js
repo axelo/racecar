71bb5664ba4d0d3a0c46c23cbb0528ea319344af
axols <axols@raa.se>
2015-09-30 21:54:06 +0200

    Introduced game states

--- src/racecar.js
+++ src/racecar.js
@@ -10,6 +10,12 @@ function Racecar() {
   var IMAGE_FONT = 'images/font.png';
   var IMAGE_MASK = 'images/mask.png';
 
+  var GAME_STATE_IDLE = 1;
+  var GAME_STATE_WAIT_FOR_PLAYER = 2;
+  var GAME_STATE_RUNNING = 3;
+  var GAME_STATE_CRASHED = 4;
+  var GAME_STATE_GAMEOVER = 5;
+
   var keyb = Keyboard();
 
   var canvas = Canvas('canvas', SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -52,9 +58,26 @@ function Racecar() {
 
   var sprites = Sprites();
 
+  var gameState = GAME_STATE_IDLE;
+
   var gameMode = 0;
   
   var score = 0;
+
+  var player = {
+    c: 2
+  };
+
+  var states = [];
+
+  states[GAME_STATE_IDLE] = {
+    clock: {
+      hh: [0, 1],
+      mm: [2, 3],
+      seconds: 0,
+      showClockSeparator: true
+    }
+  }
 
   Resources().load([IMAGE_MASK],
     function (err, url) {
@@ -69,12 +92,6 @@ function Racecar() {
     });
 
   function startGame(resourceMap) {
-
-    var player = {
-      c: 2,
-      r: 4
-    };
-
     console.log('Starting the game!');
 
     gameLoop();
@@ -88,7 +105,33 @@ function Racecar() {
     }
 
     function update(dt) {
-      //updateLevel(dt);
+      switch (gameState) {
+        case GAME_STATE_IDLE: return updateGameStateIdle(dt);
+        case GAME_STATE_RUNNING: return updateGameStateRunning(dt);
+        default: console.error('Unown game state', gameState);
+      }
+    }
+
+    function updateGameStateIdle(dt) {
+      var state = states[GAME_STATE_IDLE];
+
+      var now = new Date();
+      var hh = now.getHours();
+      var mm = now.getMinutes();
+      var seconds = now.getSeconds();
+
+      state.clock.hh[0] = Math.floor(hh / 10);
+      state.clock.hh[1] = hh - state.clock.hh[0] * 10;
+      state.clock.mm[0] = Math.floor(mm / 10);
+      state.clock.mm[1] = mm - state.clock.mm[0] * 10;
+
+      if (state.clock.seconds != seconds) {
+        state.clock.seconds = seconds;
+        state.clock.showClockSeparator = !state.clock.showClockSeparator;
+      }
+    }
+
+    function updateGameStateRunning(dt) {
       updatePlayer(dt);
     }
 
@@ -131,17 +174,28 @@ function Racecar() {
     function draw() {
       canvas.clear(resourceMap[IMAGE_MASK]);
 
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.game[gameMode]);
-
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[0][0]);
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[1][1]);
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[2][3]);
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[3][3]);
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[4][7]);
-
-      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.player[player.c]);
+      switch (gameState) {
+        case GAME_STATE_IDLE: return drawGameStateIdle();
+        case GAME_STATE_RUNNING: return drawGameStateRunning();
+        default: console.error('Unown game state', gameState);
+      }
     }
 
+    function drawGameStateIdle() {
+      var state  = states[GAME_STATE_IDLE];
+
+      if (state.clock.hh[0]) canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[0][state.clock.hh[0]]);
+      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[1][state.clock.hh[1]]);
+      
+      if (state.clock.showClockSeparator) canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.colon);
+
+      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[2][state.clock.mm[0]]);
+      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[3][state.clock.mm[1]]);
+    }
+
+    function drawGameStateRunning() {
+
+    }
   }
 
 }
