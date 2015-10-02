fcc465e63ae360cd6ff1f2dbb97a7f67b8a5453e
axols <axols@raa.se>
2015-10-02 19:58:00 +0200

    Clock idle mode, opponents acts as second indicators

--- src/racecar.js
+++ src/racecar.js
@@ -46,11 +46,11 @@ function Racecar() {
     [0,0,0,0,0],
     [1,1,0,1,1],
     [0,0,0,0,0],
-    [1,0,0,1,1],
     [0,0,0,0,0],
-    [1,1,0,0,1],
-    [0,0,0,0,0],
-    [0,1,1,0,0],
+    [0,0,1,1,0],
+    [0,0,0,1,0],
+    [0,1,0,1,0],
+    [0,1,0,1,0],
   ];
   var levelRow = level.length;
   var levelSpeed = 1000/60 * 60 * 1;
@@ -76,7 +76,17 @@ function Racecar() {
       mm: [2, 3],
       seconds: 0,
       showClockSeparator: true
-    }
+    },
+    player: 2,
+    opponents: [
+      0,0,0,0,0,
+      0,0,0,0,0,
+      0,0,0,0,0,
+      0,0,0,0,0
+    ],
+    playerCrashed: false,
+    spawnTime: 1000,
+    deltaTime: 0
   }
 
   Resources().load([IMAGE_MASK],
@@ -97,11 +107,11 @@ function Racecar() {
     gameLoop();
 
     function gameLoop() {
-      update(1000/60);
+      update(250);
       draw();
 
       //setTimeout(gameLoop, 1000/60);
-      setTimeout(gameLoop, 500);
+      setTimeout(gameLoop, 250);
     }
 
     function update(dt) {
@@ -128,6 +138,52 @@ function Racecar() {
       if (state.clock.seconds != seconds) {
         state.clock.seconds = seconds;
         state.clock.showClockSeparator = !state.clock.showClockSeparator;
+      /*}
+      state.deltaTime += dt;
+
+      if (state.deltaTime >= state.spawnTime) {
+        state.deltaTime = 0;*/
+
+        /*for (var i = state.opponents.length - 1; i >= 5; --i) {
+          state.opponents[i] = state.opponents[i - 5];
+        }
+
+        var unz = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 28]
+        var index = Math.floor(Math.random() * unz.length);
+      
+        for (var j = 0; j < 5; ++j) {
+          state.opponents[j] = (unz[index] >>> j) & 1;
+        }*/
+
+        if (state.opponents[5*3+state.player] === 1) {
+          var mindist = 5;
+          var safeIndex = -1;
+
+          for (var k = 5*3; k < 5*4; ++k) {
+            if (state.opponents[k] === 0) {
+              var dist = Math.abs(state.player - (k % 5));
+              if (dist < mindist) {
+                mindist = dist;
+                safeIndex = k % 5;
+              }
+              else if (dist === mindist) {
+                if (Math.random() >= 0.5) safeIndex = k % 5;
+              }
+            } 
+          }
+
+          if (safeIndex >= 0) state.player += safeIndex < state.player ? -1 : 1;
+        }
+
+        state.playerCrashed = state.opponents[5*3+state.player] === 1;
+
+        for (var i = state.opponents.length - 1; i >= 5; --i) {
+          state.opponents[i] = state.opponents[i - 5];
+        }
+
+        for (var j = 0; j < 5; ++j) {
+          state.opponents[4 - j] = (state.clock.seconds >>> j) & 1;
+        }
       }
     }
 
@@ -191,6 +247,22 @@ function Racecar() {
 
       canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[2][state.clock.mm[0]]);
       canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.segment[3][state.clock.mm[1]]);
+
+      if (state.clock.seconds < 32) canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.game[0]);
+      else canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.game[1]);
+
+      for (var i = 0; i < state.opponents.length; ++i) {
+        if (state.opponents[i] === 1) {
+          canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.opponent[Math.floor(i / 5)][i % 5]);
+        }
+      }
+
+      if (state.playerCrashed) {
+        //canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.opponent[3][state.player]);
+        canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.crash[state.player]);
+      }
+
+      canvas.drawSprites(resourceMap[IMAGE_MASK], sprites.player[state.player]);
     }
 
     function drawGameStateRunning() {
